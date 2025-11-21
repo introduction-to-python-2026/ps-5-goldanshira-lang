9382a45366397fadc3b1f541471e3407d90595f2
goldanshira-lang <goldanshira@gmail.com>
2025-11-21 17:55:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-# Add the import statements for functions from string_utils.py and equation_utils.py here
+from equation_utils.py import ELEMENTS,generate_equation_for_element,build_equations,my_solve
+from string_utils.py import split_by_capitals,split_by_capitals,split_by_capitals,parse_chemical_reaction,parse_chemical_reaction
 
 
 def balance_reaction(reaction): #"Fe2O3 + H2 -> Fe + H2O"
