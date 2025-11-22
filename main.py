fc6957196d09bbd1d9d931ef7d749dfcf277aa6b
goldanshira-lang <goldanshira@gmail.com>
2025-11-22 12:08:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from equation_utils.py import ELEMENTS,generate_equation_for_element,build_equations,my_solve
-from string_utils.py import split_by_capitals,split_by_capitals,split_by_capitals,parse_chemical_reaction,parse_chemical_reaction
+from equation_utils import ELEMENTS,generate_equation_for_element,build_equations,my_solve
+from string_utils import split_by_capitals,split_by_capitals,split_by_capitals,parse_chemical_reaction,parse_chemical_reaction
 
 
 def balance_reaction(reaction): #"Fe2O3 + H2 -> Fe + H2O"
