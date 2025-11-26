ee39d433a91c332fb23f2757c929273fa693fb33
goldanshira-lang <goldanshira@gmail.com>
2025-11-26 18:10:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from equation_utils import *
 from string_utils import *
-
+from sympy import solve as sympy_solve
+from sympy import symbols, Eq
 
 def balance_reaction(reaction): #"Fe2O3 + H2 -> Fe + H2O"
 
