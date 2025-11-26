b8e846aa4e0bb6bd575b51511de802cf0a599499
goldanshira-lang <goldanshira@gmail.com>
2025-11-26 18:15:24 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -1,24 +1,41 @@
 
 
+def split_by_capitals(formula):
+    apperapper = []
+    new_formula = []
+    for i in range(len(formula)):
+      if formula[i].isupper() == True:
+        apperapper.append(i)
+    print(apperapper)
+    if apperapper:
+      for i in range(len(apperapper) -1):
+        print(i)
+        new_word = formula[apperapper[i]:apperapper[i+1]]
+        new_formula.append(new_word)
+      last_word = formula[apperapper[len(apperapper) - 1]:]
+      new_formula.append(last_word)
+      print(new_formula)
+    elif formula:
+      new_formula.append(formula)
+    return new_formula
 
-def split_before_uppercases(formula):
-    pass  # replace the pass with your code
-
-def split_at_digit(formula):
-    pass  # replace the pass with your code
+def split_at_number(formula):
+    num = []
+    for i in range(len(formula)):
+      if formula[i].isdigit() == True:
+        num = formula[i:]
+        letter = formula [0:i]
+        num = int(num)
+        return (letter, num)
+    return (formula ,1)
 
 def count_atoms_in_molecule(molecular_formula):
-    """Takes a molecular formula (string) and returns a dictionary of atom counts.  
-    Example: 'H2O' → {'H': 2, 'O': 1}"""
-
-    # Step 1: Initialize an empty dictionary to store atom counts
-
+    dic = {}
     for atom in split_by_capitals(molecular_formula):
-        atom_name, atom_count = split_at_number(atom)
-        
-        # Step 2: Update the dictionary with the atom name and count
+        atom_name, atom_count = split_at_number(atom)      
+        dic [atom_name] = atom_count
+    return dic
 
-    # Step 3: Return the completed dictionary
 
 
 
